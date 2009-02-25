0df35441e277bea736ac6d32bcaa5eda2eede6ae
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2009-02-25 21:32:49 +0000

    Specs are running but failing as expected now
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/frameworks/taza@340519 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/site_fixtures_spec.rb
+++ spec/site_fixtures_spec.rb
@@ -2,13 +2,14 @@ require 'spec/spec_helper'
 require 'taza/fixture'
 
 describe "Site Specific Fixtures" do
-  include Taza::Fixtures::FooSite
- 
+
   it "should be able to access fixtures in sub-folders" do
+    self.extend Taza::Fixtures::FooSite
     bars(:foo).name.should eql("foo")
   end
 
   it "should not be able to access non-site-specific fixtures" do
+    self.extend Taza::Fixtures::FooSite
     lambda{foos(:gap)}.should raise_error(NoMethodError)
   end
 
