e01d0ec077a2239dd1c95d349afc223094d15a86
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2009-02-25 21:32:56 +0000

    No more dos line endings thank you.
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/frameworks/taza@340523 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -4,11 +4,11 @@ require 'taza/settings'
 require 'taza/site'
 
 describe Taza::Settings do
-  
+
   before :all do
     @site_name = 'SiteName'
   end
-  
+
   before :each do
     ENV['TAZA_ENV'] = 'isolation'
     ENV['BROWSER'] = nil
@@ -20,19 +20,19 @@ describe Taza::Settings do
     ENV['BROWSER'] = 'foo'
     Taza::Settings.config(@site_name)[:browser].should eql(:foo)
   end
-  
+
   it "should provide default values if no config file or environment settings provided" do
     Taza::Settings.stubs(:path).returns("spec/sandbox")
     Taza::Settings.config(@site_name)[:driver].should eql(:selenium)
     Taza::Settings.config(@site_name)[:browser].should eql(:firefox)
   end
-  
+
   it "should use environment variable for driver settings" do
     Taza::Settings.stubs(:path).returns("spec/sandbox")
     ENV['DRIVER'] = 'bar'
     Taza::Settings.config(@site_name)[:driver].should eql(:bar)
   end
-  
+
   it "should be able to load the site yml" do
     Taza::Settings.stubs(:path).returns("spec/sandbox")
     Taza::Settings.config("SiteName")[:url].should eql('http://google.com')
@@ -61,20 +61,20 @@ describe Taza::Settings do
     Taza::Settings.stubs(:path).returns("spec/sandbox")
     Taza::Settings.stubs(:config_file_path).returns('spec/sandbox/config.yml')
   end
-  
+
   it "should raise error for a config file that doesnot exist" do
     Taza::Settings.stubs(:path).returns('spec/sandbox/file_not_exists.yml')
     lambda {Taza::Settings.config}.should raise_error
   end
-  
+
   it "should path point at root directory" do
     Taza::Settings.path.should eql('.')
   end
-  
+
   it "should use the config file's variable for driver settings if no environment variable is set" do
     Taza::Settings.stubs(:path).returns("spec/sandbox")
     Taza::Settings.stubs(:config_file).returns({:driver => :fun})
-    Taza::Settings.config(@site_name)[:driver].should eql(:fun)    
+    Taza::Settings.config(@site_name)[:driver].should eql(:fun)
   end
 
   class SiteName < Taza::Site
