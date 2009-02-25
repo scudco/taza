7917343f7a832400419c300b9e34fd3d782a7c07
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2009-02-25 21:32:52 +0000

    fixture specs are all correct... and failing still can't mock base_path
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/frameworks/taza@340521 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_spec.rb
+++ spec/fixture_spec.rb
@@ -2,55 +2,46 @@ require 'spec/spec_helper'
 require 'taza'
 
 describe Taza::Fixture do
-  
+  before :each do
+    @base_path = File.join('.','spec','sandbox','fixtures','')
+  end
+
   it "should be able to load entries from fixtures" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','*.yml'))
+    fixture.load_fixtures_from(@base_path)
     example = fixture.get_fixture_entity(:examples,'first_example')
     example.name.should eql("first")
     example.price.should eql(1)
   end
 
-  it "should use the spec folder as the base path" do
-    Taza::Fixture.new.base_path.should eql('./spec')
+  it "should use the spec fixtures folder as the base path" do
+    Taza::Fixture.base_path.should eql('./spec/fixtures/')
   end
 
   it "should know if a pluralized fixture of that name exists" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','*.yml'))
+    fixture.load_fixtures_from(@base_path)
     fixture.pluralized_fixture_exists?('example').should be_true
     fixture.pluralized_fixture_exists?('boo').should be_false
   end
   
   it "should be able to get all fixtures loaded excluding sub-folder fixtures" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','*.yml'))
+    fixture.load_fixtures_from(@base_path)
     fixture.fixture_names.should be_equivalent([:examples,:users,:foos])
   end
   
-  it "should be able to get all fixtures loaded including sub-folder fixtures" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
-    fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','**','*.yml'))
-    fixture.fixture_names.should be_equivalent([:examples,:users,:foos,:bars])
-  end
-
   it "should be able to get specific fixture entities" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','*.yml'))
+    fixture.load_fixtures_from(@base_path)
     examples = fixture.specific_fixture_entities(:examples, ['third_example'])
     examples.length.should eql(1)
     examples['third_example'].name.should eql('third')
   end
 
   it "should not modified the fixtures when you get specific entities off a fixture" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
-    fixture.load_all(File.join('fixtures','*.yml'))
+    fixture.load_fixtures_from(@base_path)
     previous_count = fixture.get_fixture(:examples).length
     examples = fixture.specific_fixture_entities(:examples, ['third_example'])
     fixture.get_fixture(:examples).length.should eql(previous_count)
--- spec/fixtures_spec.rb
+++ spec/fixtures_spec.rb
@@ -2,7 +2,7 @@ require 'spec/spec_helper'
 require 'taza/fixture'
 
 describe Taza::Fixtures do
-  Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')  
+  Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox/fixtures/')  
   include Taza::Fixtures
   
   it "should be able to look up a fixture entity off fixture_methods module" do
