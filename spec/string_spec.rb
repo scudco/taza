63a370063ac05c4e91cb40a58e307c92469e4f69
cbaker <cbaker@d1bde68c-af00-0410-813c-e860f6c31c7f>
2009-08-26 16:01:40 +0000

    Adding spec for variablize - convert human readable words into variable syntax.
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/frameworks/taza@367965 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -5,4 +5,8 @@ describe "string extensions" do
   it "should pluralize and to sym a string" do
     "apple".pluralize_to_sym.should eql(:apples)
   end
+  
+  it "should variablize words with spaces" do
+    "foo -  BAR".variablize.should eql("foo_bar")
+  end	
 end
