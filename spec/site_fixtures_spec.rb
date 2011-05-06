a6e3b3759246f0b22510a7bf7d505cd4c250b572
Pedro Nascimento <pnascimento@gmail.com>
2011-05-05 21:18:20 -0300

    This is something I want to run before every test, not on the spec context

--- spec/site_fixtures_spec.rb
+++ spec/site_fixtures_spec.rb
@@ -1,8 +1,10 @@
 require 'spec/spec_helper'
-
+require 'ruby-debug'
 describe "Site Specific Fixtures" do
+  before do
   Taza::Fixture.stubs(:base_path).returns(File.join('.','spec','sandbox','fixtures',''))
   include Taza::Fixtures::FooSite
+  end
 
   it "should be able to access fixtures in sub-folders" do
     bars(:foo).name.should eql("foo")
@@ -11,5 +13,4 @@ describe "Site Specific Fixtures" do
   it "should not be able to access non-site-specific fixtures" do
     lambda{foos(:gap)}.should raise_error(NoMethodError)
   end
-
 end
