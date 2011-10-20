be1d8ee98ca5f25728269f451ec8402728ae4f7f
Pedro Nascimento <pnascimento@gmail.com>
2011-10-20 00:23:59 -0200

    Cant load the spec_helper anymore.

--- spec/project_generator_spec.rb
+++ spec/project_generator_spec.rb
@@ -29,20 +29,6 @@ describe "Project Generator" do
     system("ruby -c #{@spec_helper} > #{null_device}").should be_true
   end
 
-  it "spec helper should set the TAZA_ENV variable if it is not provided" do
-    ENV['TAZA_ENV'] = nil
-    run_generator('taza', [APP_ROOT], generator_sources)
-    load @spec_helper
-    ENV['TAZA_ENV'].should eql("isolation")
-  end
-
-  it "spec helper should not override the TAZA_ENV variable if was provided" do
-    ENV['TAZA_ENV'] = 'orange pie? is there such a thing?'
-    run_generator('taza', [APP_ROOT], generator_sources)
-    load @spec_helper
-    ENV['TAZA_ENV'].should eql('orange pie? is there such a thing?')
-  end
-
   it "should generate a console script" do
     run_generator('taza', [APP_ROOT], generator_sources)
     File.exists?(File.join(APP_ROOT,'script','console')).should be_true
